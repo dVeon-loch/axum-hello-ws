40d85d7236fafc65ff95a96f42ebaa85ddcf5795
dVeon-loch <dveonloch@protonmail.com>
2026-03-25 15:16:35 +0200

    feat: Add env var port specification

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,19 @@ use axum::{
 
 #[tokio::main]
 async fn main() {
+    let port = std::env::args()
+        .nth(1)
+        .or_else(|| std::env::var("PORT").ok())
+        .and_then(|p| p.parse::<u16>().ok())
+        .unwrap_or(8080);
+
     let app = Router::new()
         .route("/", get(hello))
         .route("/ws", get(ws_handler));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
-    println!("Listening on http://0.0.0.0:8080");
+    let addr = format!("0.0.0.0:{port}");
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    println!("Listening on http://{addr}");
     axum::serve(listener, app).await.unwrap();
 }
 
